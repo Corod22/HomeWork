f6be9193bf512044b631c10fea2875e5d7dcc4ef
Alexander <continenttal2015@yandex.ru>
2023-03-03 19:48:04 +0300

    Improved task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,20 +4,27 @@
 // 14212->нет
 // 12821->да
 // 23432->да
-Console.WriteLine("Введите пятизначное число");
+Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 int num = number;
 int result = 0;
 int a = 0;
-int tK = 10000;
-if (num < 100000)
+
+int digits = 0; 
+int number2=number;
+while (number2 > 0)
 {
-    while (tK>0)
+  number2 = number2/10;
+  digits++;
+}
+int dig = ((int)Math.Pow(10, digits))/10;
+
+    while (dig>0)
     {
     a = num % 10;
     num = num / 10;
-    result = result + a * tK;
-    tK=tK/10;
+    result = result + a * dig;
+    dig=dig/10;
     }
     if (number==result)
     {
@@ -27,13 +34,6 @@ if (num < 100000)
     {
         Console.WriteLine($"Число {number} -> нет, не палидром");
     }
-    // Console.WriteLine($"Палидром числа {number} - > {result}");
-
-}
-else
-{
-    Console.WriteLine("Введите пятизначное число");
-}
 
 
 Console.Read();
